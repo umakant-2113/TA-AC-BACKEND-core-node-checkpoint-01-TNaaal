f16e0b5e912df408d10622dcc86b401d7a1f692f
umakant-2113 <lodhiumakant800@gmail.com>
2022-05-02 06:57:18 +0530

    Done

--- block-BNaaep/assets/index.js
+++ block-BNaaep/assets/index.js
@@ -3,9 +3,33 @@ let url=require('url');
 let qs =require('querystring')
 let http=require('http');
 let fs=require('fs');
-// make server
 let httmPath=path.join(__dirname,"/index.html")
 let htmlAbout=path.join(__dirname,"/about.html")
+// make server
+// index png render 
+let server1=http.createServer(hendleIndexPng);
+function hendleIndexPng(req,res){
+if(req.url==="/"&&req.method==="GET"){
+  res.setHeader('content-type','image/jpg');
+  fs.readFile('./index.png',(err,content)=>{
+    if(err) return console.log(err);
+    res.write(content);
+    res.end()
+  })
+}else if(req.url==="/about" &&req.method==="GET"){
+res.setHeader('content-type','image/png');
+fs.readFile("./about.png",(err,content)=>{
+  if(err) return console.log(err);
+  res.write(content);
+  res.end()
+})
+}
+}
+server1.listen(3333,()=>{
+  console.log('we are local hsot 3333 in render index png ')
+})
+
+// index html render
 let server=http.createServer(handleServer)
 function handleServer(req,res){
 if(req.method==='GET'&&req.url==="/"){
@@ -15,6 +39,7 @@ if(req.method==='GET'&&req.url==="/"){
      res.write(content);
      res.end();
  })
+//  about html render
 }else if(req.method==="GET" &&req.url==="/about"){
     res.setHeader('content-type','text/html');
     fs.readFile(htmlAbout,(err,content)=>{
@@ -22,6 +47,7 @@ if(req.method==='GET'&&req.url==="/"){
         res.write(content);
         res.end();
     })
+    // css render 
 } else if(req.url.split(".").pop()==="css"){
     console.log(req.url ,'first')
     res.setHeader('Content-Type','text/css');
@@ -30,6 +56,7 @@ if(req.method==='GET'&&req.url==="/"){
        res.write(content)
        res.end();
      })
+    //  image render
    }else if(req.url.split(".").pop()==="jpg"){
     console.log(req.url,'second')
     res.setHeader('Content-Type','image/jpg');
@@ -52,6 +79,7 @@ let store="";
 req.on('data',(chunk)=>{
   store+=chunk
 })
+// contect html render by get method 
 req.on('end',()=>{
   if(req.method==="GET"&&req.url==="/contect"){
     let contectPath=path.join(__dirname,"..","/contacts","/contect.html");
@@ -61,6 +89,7 @@ req.on('end',()=>{
       res.write(content);
       res.end();
     })
+    // contect html render by post method 
   }else if(req.method==="POST"&&req.url==="/form"){
     let paeseData=JSON.stringify(qs.parse(store))
     let username=qs.parse(store).username
@@ -74,7 +103,7 @@ req.on('end',()=>{
         fs.close(fd, (err) => {
           res.write(`<h1>${qs.parse(store).username}</h1><h2>${qs.parse(store).name}</h2> <h2>${qs.parse(store).email}</h2><h2>${qs.parse(store).bio}</h2>
           <h2>${qs.parse(store).age}</h2>`)
-          res.end(`${username} successfully created`);
+          res.end();
         });
       });
     });
